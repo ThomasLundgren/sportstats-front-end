f8a5ca865c99372cf6cfe38416fc73754383bfb6
NiklasNordgren <39832486+NiklasNordgren@users.noreply.github.com>
2019-10-16 09:16:57 +0200

    navbar tests

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,19 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { GetSportsService } from './service/get-sports-service.service';
+import { Sport } from './model/sport.model';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'sportstats-system';
+  sports: Sport[] = [];
+  tests: number[] = [1, 2, 3, 4];
 
-  constructor(private service: GetSportsService) { }
+  constructor(private getSportsService: GetSportsService) { }
 
   ngOnInit() {
-    this.service.getSports()
-    .subscribe( data => { this.service.setSports(data); console.log(data); });
+    this.getSports();
+  }
+
+  getSports(): void {
+    this.getSportsService.getSports()
+      .subscribe(data => {
+        this.sports = data;
+        console.log(data);
+        //this.getSportsService.setSports(data); 
+      });
   }
 
 }
