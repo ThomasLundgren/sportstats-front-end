50db4fee400d89015b4958b77dfbc30662e8baf4
Thomas Lundgren <thomasLundgren@live.com>
2019-10-17 14:33:20 +0200

    Update SportComponent
    SportComponent now fetches the Sport model object that has been clicked.
    Use {{sport.name}} in sport.component.html

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,15 +1,14 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
-import { SportComponent } from './component/sport/sport.component';
+import { NgModule } from "@angular/core";
+import { Routes, RouterModule } from "@angular/router";
+import { SportComponent } from "./component/sport/sport.component";
 import { Sport } from "./model/sport.model";
 
-
 const routes: Routes = [
-  { 
-    path: 'sport/:id',
+  {
+    path: "sport/:id",
     component: SportComponent,
     data: Sport
-   },
+  }
 ];
 
 @NgModule({
@@ -17,11 +16,7 @@ const routes: Routes = [
   exports: [RouterModule]
 })
 export class AppRoutingModule {
-
   sports: Sport[];
 
-  constructor() {
-
-  }
-
- }
+  constructor() {}
+}
--- src/app/component/sport/sport.component.ts
+++ src/app/component/sport/sport.component.ts
@@ -1,22 +1,26 @@
-import { Component, OnInit } from "@angular/core";
+import { Component, OnInit, Input, OnChanges, SimpleChanges } from "@angular/core";
 import { Sport } from "src/app/model/sport.model";
 import { Router, ActivatedRoute } from "@angular/router";
-import { SportService } from 'src/app/service/sport.service';
+import { SportService } from "src/app/service/sport.service";
 
 @Component({
   selector: "app-sport",
   templateUrl: "./sport.component.html",
-  styleUrls: ["./sport.component.scss"]
+  styleUrls: ["./sport.component.scss"],
 })
 export class SportComponent implements OnInit {
-  sport: Sport
+  sport: Sport = {
+    id: 0,
+    name: "",
+    leagues: []
+  };
 
   constructor(private route: ActivatedRoute, private sportService: SportService) {}
 
   ngOnInit() {
     this.route.paramMap.subscribe(params => {
       let sportId = parseInt(params.get("id"));
-      console.log(sportId);
+
       this.sportService.getSports().subscribe(sports => {
         this.sport = sports.filter(sport => sport.id === sportId).pop();
       });
