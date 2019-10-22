ff174c5a96aa914e57c3f5b5b418e60aa6281cd7
NiklasNordgren <39832486+NiklasNordgren@users.noreply.github.com>
2019-10-22 18:11:12 +0200

    Update admin

--- src/app/component/admin/admin.component.ts
+++ src/app/component/admin/admin.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { faPlusSquare, faUserEdit } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-admin',
@@ -6,6 +7,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./admin.component.scss']
 })
 export class AdminComponent implements OnInit {
+
+  faPlusSquare = faPlusSquare;
+  faUserEdit = faUserEdit;
 
   constructor() { }
 
