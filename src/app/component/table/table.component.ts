b8d10f8fed2a9406777951cd7d6ff445b63489a4
SannaLundqvist <lundqvist174@gmail.com>
2019-10-24 13:18:57 +0200

    table for league
    Woriking!

--- src/app/component/table/table.component.ts
+++ src/app/component/table/table.component.ts
@@ -1,8 +1,10 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, OnChanges, SimpleChanges, OnDestroy } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { TableService } from 'src/app/service/table.service';
 import { Table } from 'src/app/model/table.model';
 import { Season } from 'src/app/model/season.model';
+import { TableRow } from 'src/app/model/table-row.model';
+import { Subscription } from 'rxjs';
 
 
 @Component({
@@ -10,20 +12,43 @@ import { Season } from 'src/app/model/season.model';
   templateUrl: './table.component.html',
   styleUrls: ['./table.component.css']
 })
-export class TableComponent implements OnInit {
-  @Input() seasonId : number;
+export class TableComponent implements OnInit, OnChanges, OnDestroy {
+
+  @Input() seasonId: number;
 
   season: Season;
+  subscriptions: Subscription[] = [];
+  tableDummy: Table = {
+    season: {
+      id: 0,
+      startDate: new Date(),
+      endDate: new Date(),
+      leagueId: 0,
+      roundLimit: 10
+    },
+    tableRows: []
+  }
   table: Table;
+
   constructor(private route: ActivatedRoute, private tableService: TableService) { }
 
-  ngOnInit() {
-    this.route.paramMap.subscribe(params => {
-      this.tableService.getTableBySeasonId(this.seasonId).subscribe(data => {
-        console.log(data);
+  ngOnChanges(changes: SimpleChanges) {
+    if (this.seasonId === -1)
+      this.table = this.tableDummy;
+    else {
+      this.subscriptions.push(this.tableService.getTableBySeasonId(this.seasonId).subscribe(data => {
         this.table = data;
-      });
-    });
+      }));
+    }
   }
 
+  ngOnInit() {
+    this.table = this.tableDummy;
+  }
+
+  ngOnDestroy(): void {
+    this.subscriptions.forEach(sub => {
+      sub.unsubscribe();
+    });
+  }
 }
