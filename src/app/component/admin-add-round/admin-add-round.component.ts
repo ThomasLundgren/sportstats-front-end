22c91847426b8f97a6a2355cce0a91831eff6846
NiklasNordgren <39832486+NiklasNordgren@users.noreply.github.com>
2019-10-24 20:31:45 +0200

    Update admin-add-round.component

--- src/app/component/admin-add-round/admin-add-round.component.ts
+++ src/app/component/admin-add-round/admin-add-round.component.ts
@@ -1,4 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { Sport } from 'src/app/model/sport.model';
+import { League } from 'src/app/model/league.model';
+import { Season } from 'src/app/model/season.model';
+import { SportService } from 'src/app/service/sport.service';
+import { LeagueService } from 'src/app/service/league.service';
+import { FormGroup, FormControl } from '@angular/forms';
+import { SeasonService } from 'src/app/service/season.service';
 
 @Component({
   selector: 'app-admin-add-round',
@@ -7,9 +14,63 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AdminAddRoundComponent implements OnInit {
 
-  constructor() { }
+  sports: Sport[] = [];
+  leagues: League[] = [];
+  seasons: Season[] = [];
+
+  addRoundForm = new FormGroup({
+
+  });
+
+  constructor(
+    private sportService: SportService,
+    private leagueService: LeagueService,
+    private seasonService: SeasonService
+  ) { }
 
   ngOnInit() {
+    this.getSports();
+  }
+
+  getSports(): void {
+    this.sportService.getSports().subscribe(data => {
+      this.sports = data;
+    });
+  }
+
+  getLeaguesBySportId(sportId: number): void {
+    this.leagueService.getLeaguesBySportId(sportId).subscribe(data => {
+      this.leagues = data;
+    });
+  }
+
+  getSeasonByLeagueId(leagueId: number): void{
+    this.seasonService.getSeasonsByLeagueId(leagueId).subscribe(data => {
+      this.seasons = data;
+    })
+  }
+
+  onSubmit(): void {
+    this.addRoundForm.controls.roundLimit.reset();
+    //this.addSeasonForm.controls.startDate.reset();
+    //this.addSeasonForm.controls.endDate.reset();
+  }
+
+  onReset(): void {
+    this.sports = [];
+    this.leagues = [];
+    this.seasons = [];
+  }
+
+  sportChanged(sportId: number): void {
+    this.getLeaguesBySportId(sportId);
+    this.leagues = [];
+    this.seasons = [];
+  }
+
+  leagueChanged(leagueId: number): void {
+    this.getSeasonByLeagueId(leagueId);
+    this.seasons = [];
   }
 
 }
